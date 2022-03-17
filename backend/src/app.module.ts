7a41ea139292b8e4408786d2a25b9ea31146561f
catherine meng <cmeng@catherines-MacBook-Pro.local>
2022-03-17 14:36:15 -0700

    backend/feature: added an openapi record example

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecordsModule } from './records/records.module';
 
 @Module({
-  imports: [],
+  imports: [RecordsModule],
   controllers: [AppController],
   providers: [AppService],
 })
