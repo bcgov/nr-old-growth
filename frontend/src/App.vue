a92b51bcd5c40d3c4f8df0a10170268e779dba2c
Maria Martinez <maria.martinez@gov.bc.ca>
2022-03-25 16:50:48 -0700

    feat: added menu bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,9 @@
   <div>
     <MainHeader />
     <b-container>
-     <b-row>
-        <b-col col md="2"></b-col>
-        <b-col col md="10"><MapViewer /></b-col>
+      <b-row>
+        <b-col col sm="1" md="2" lg="2" xl="2"><MenuBar /></b-col>
+        <b-col col sm="11" md="10" lg="10" xl="10"><MapViewer /></b-col>
       </b-row>
     </b-container>
   </div>
@@ -14,6 +14,7 @@
 import { defineComponent } from "vue";
 import MainHeader from "./components/MainHeader.vue";
 import MapViewer from "./components/MapViewer.vue";
+import MenuBar from "./components/MenuBar.vue";
 import "leaflet/dist/leaflet.css";
 import "./assets/global.css";
 
@@ -22,6 +23,7 @@ export default defineComponent({
   components: {
     MainHeader,
     MapViewer,
+    MenuBar,
   },
 });
 </script>
@@ -35,20 +37,25 @@ export default defineComponent({
   color: #2c3e50;
 }
 
-.row {
-  padding: 0px 0px 0px 0px;
-  margin: 0px 0px 0px 0px;
-}
-
-.container, .container-fluid, .container-xxl, .container-xl, .container-lg, .container-md, .container-sm {
+.row > * {
   padding: 0px 0px 0px 0px !important;
   margin: 0px 0px 0px 0px !important;
-  max-width: 99% !important;
+}
+
+.container,
+.container-fluid,
+.container-xxl,
+.container-xl,
+.container-lg,
+.container-md,
+.container-sm {
+  padding: 0px 0px 0px 0px !important;
+  margin: 0px 0px 0px 0px !important;
+  max-width: 98% !important;
 }
 
 .col {
   padding: 0px 0px 0px 0px;
   margin: 0px 0px 0px 0px;
 }
-
 </style>
