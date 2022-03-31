6ba5a08edae1e2a117367d0e9332da7cbdca69a3
catherine meng <cmeng@catherines-MacBook-Pro.local>
2022-03-31 15:23:40 -0700

    config: config different environment for both frontend and backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,35 @@
 import { fileURLToPath, URL } from "url";
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  // test: {
-  //   globals: true,
-  // },
-  server: {
-    port: 8080,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000/",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const port = parseInt(env.VITE_PORT) || 8080;
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
+    // test: {
+    //   globals: true,
+    // },
+    server: {
+      port: port,
+      proxy: {
+        "/api": {
+          target:
+            env.VITE_NODE_ENV === "development"
+              ? "http://localhost:3000/"
+              : "https://gfp-backend-dev.apps.silver.devops.gov.bc.ca",
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
