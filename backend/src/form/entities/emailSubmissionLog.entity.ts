a959e8f3c3252c0c25b51aa628a508db9c78c979
Maria Martinez <maria.martinez@gov.bc.ca>
2022-06-09 10:04:41 -0700

    fix/be: Added back ApiPropertyOptional annotations

--- backend/src/form/entities/emailSubmissionLog.entity.ts
+++ backend/src/form/entities/emailSubmissionLog.entity.ts
@@ -7,12 +7,24 @@ export class EmailSubmissionLogEntity extends BaseEntity {
   @PrimaryGeneratedColumn({ name: 'email_submission_log_id' })
   id: number;
 
+  @ApiPropertyOptional({
+    example: 'DELIVERED',
+    description: 'The code indicates if the email sent out successfully',
+  })
   @Column({ name: 'code' })
   code: string;
 
+  @ApiPropertyOptional({
+    example: '',
+    description: 'The error messages',
+  })
   @Column({ name: 'exception_log' })
   exceptionLog: string;
 
+  @ApiPropertyOptional({
+    example: '23F86A29',
+    description: 'The confirmation id of a form submission',
+  })
   @Column({ name: 'confirmation_id' })
   confirmationId: string;
 
