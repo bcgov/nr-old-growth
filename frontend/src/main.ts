77d14c501cb77bac4fe0183865aaffc7a5b59e2d
catherine meng <cmeng@catherines-MacBook-Pro.local>
2022-06-27 11:10:41 -0700

    feat/frontend: explore keycloak login, refs: #67

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
-import VueKeycloakJs from "@dsb-norge/vue-keycloak-js";
 import App from "./App.vue";
 import BootstrapVue3 from "bootstrap-vue-3";
 import { BootstrapIconsPlugin } from "bootstrap-icons-vue";
+import VueKeycloakJs from "@dsb-norge/vue-keycloak-js";
 import type { KeycloakInstance } from "keycloak-js";
 import type { VueKeycloakInstance } from "@dsb-norge/vue-keycloak-js/dist/types";
 
@@ -10,30 +10,27 @@ import type { VueKeycloakInstance } from "@dsb-norge/vue-keycloak-js/dist/types"
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-const app = createApp(App);
+const app = createApp(App).use(VueKeycloakJs, {
+  init: {
+    // Use 'login-required' to always require authentication
+    // If using 'login-required', there is no need for the router guards in router.js
+    onLoad: "login-required",
+    // checkLoginIframe: false,
+    // onLoad: "check-sso",
+    // silentCheckSsoRedirectUri:
+    //   window.location.origin + "/silent-check-sso.html",
+  },
+  config: {
+    url: "https://dev.oidc.gov.bc.ca/auth/",
+    clientId: "nrog",
+    realm: "ichqx89w",
+  },
+  onReady(keycloak: KeycloakInstance) {
+    console.log("Keycloak ready", keycloak);
+  },
+});
 app.use(BootstrapIconsPlugin);
-app.use(BootstrapVue3);
-
-app
-  .use(VueKeycloakJs, {
-    init: {
-      // Use 'login-required' to always require authentication
-      // If using 'login-required', there is no need for the router guards in router.js
-      onLoad: "login-required",
-      // onLoad: "check-sso",
-      // silentCheckSsoRedirectUri:
-      //   window.location.origin + "/silent-check-sso.html",
-    },
-    config: {
-      url: "https://dev.oidc.gov.bc.ca",
-      clientId: "nrog",
-      realm: "Ichqx89w",
-    },
-    onReady(keycloak: KeycloakInstance) {
-      console.log("Keycloak ready", keycloak);
-    },
-  })
-  .mount("#app");
+app.use(BootstrapVue3).mount("#app");
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
