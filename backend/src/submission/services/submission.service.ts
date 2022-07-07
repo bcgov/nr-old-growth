05126fc65ff85e01061e88cf4a3e70c06aca40ff
Maria Martinez <maria.martinez@gov.bc.ca>
2022-07-07 15:01:35 -0700

    feat/database: Added rollback functionality

--- backend/src/submission/services/submission.service.ts
+++ backend/src/submission/services/submission.service.ts
@@ -1,20 +1,15 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { Cron } from "@nestjs/schedule";
-import { InjectRepository } from "@nestjs/typeorm";
-import { CutblockSubmissionDetailsEntity } from "src/cutblocksubmissiondetails/entities/cutblockSubmissionDetails.entity";
-import { SubmissionEntity } from "src/submission/entities/submission.entity";
-import { Repository } from "typeorm";
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { CutblockSubmissionDetailsEntity } from 'src/cutblocksubmissiondetails/entities/cutblockSubmissionDetails.entity';
+import { SubmissionEntity } from 'src/submission/entities/submission.entity';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class SubmissionService {
   private readonly logger = new Logger(SubmissionService.name);
 
   constructor(
-    @InjectRepository(SubmissionEntity)
-    private submissionRepository: Repository<SubmissionEntity>,
-
-    @InjectRepository(CutblockSubmissionDetailsEntity)
-    private cutblockSubmissionDetailsRepository: Repository<CutblockSubmissionDetailsEntity>,
+    private dataSource: DataSource,
   ) {}
 
   findAll() {
@@ -31,42 +26,42 @@ export class SubmissionService {
 
   //@Cron("*/5 * * * * *") //Runs every 5 seconds
   async postData(): Promise<any> {
-    //TODO: Add rollback if any save fails
-    const newSubmissionEntity = new SubmissionEntity();
-    newSubmissionEntity.submitterId = 7;
-    newSubmissionEntity.licenseeName = "Test";
-    newSubmissionEntity.firstName = "Test 2";
-    newSubmissionEntity.lastName = "Test 3";
-    newSubmissionEntity.phoneNumber = "(604)";
-    newSubmissionEntity.emailAddress = "m@m.com";
-    newSubmissionEntity.nrdc = "DCC";
-    newSubmissionEntity.submissionDate = new Date();
-    newSubmissionEntity.createUser = "Test 4";
+    const queryRunner = this.dataSource.createQueryRunner();
 
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
     try {
-      const submission = 
-        await this.submissionRepository.save(
-          newSubmissionEntity,
-        );
-      console.log(JSON.stringify(submission));
+      const newSubmissionEntity = new SubmissionEntity();
+      newSubmissionEntity.submitterId = 7;
+      newSubmissionEntity.licenseeName = 'Test';
+      newSubmissionEntity.firstName = 'Test 2';
+      newSubmissionEntity.lastName = 'Test 3';
+      newSubmissionEntity.phoneNumber = '(604)';
+      newSubmissionEntity.emailAddress = 'm@m.com';
+      newSubmissionEntity.nrdc = 'DCC';
+      newSubmissionEntity.submissionDate = new Date();
+      newSubmissionEntity.createUser = 'Test 4';
+
+      const submission = await queryRunner.manager.save(newSubmissionEntity);
 
       const newCutblockSubmissionDetailsEntity = new CutblockSubmissionDetailsEntity();
       newCutblockSubmissionDetailsEntity.submissionEntity = submission;
-      newCutblockSubmissionDetailsEntity.cutBlockId = "Test 11";
+      newCutblockSubmissionDetailsEntity.cutBlockId = 'Test 11';
       newCutblockSubmissionDetailsEntity.totalBlockHa = 4.33;
       newCutblockSubmissionDetailsEntity.haOrgMappedDefArea = 33.44;
-      newCutblockSubmissionDetailsEntity.createUser = "Test 4";
+      newCutblockSubmissionDetailsEntity.createUser = 'Test 4';
       newCutblockSubmissionDetailsEntity.createTimestamp = new Date();
 
-      const cutblockSubmissionDetails =
-        await this.cutblockSubmissionDetailsRepository.save(
-          newCutblockSubmissionDetailsEntity,
-        );
-      console.log(JSON.stringify(cutblockSubmissionDetails));
+      await queryRunner.manager.save(newCutblockSubmissionDetailsEntity);
+      await queryRunner.commitTransaction();
     } 
-    catch (error) {
-      this.logger.error("Failed to write into db: \n" + error);
-      return null;
+    catch (err) {
+      this.logger.error("\nFailed to write into db: \n" + err + "." +
+                        "\nThe transaction has been rollbacked.");
+      await queryRunner.rollbackTransaction();
+    } 
+    finally {
+      await queryRunner.release();
     }
   }
 }
