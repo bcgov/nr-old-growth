3173decc4f0961db2eb42b0521ec2c3393580f0b
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-04-25 10:58:15 -0700

    feat: adding health check

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import axios from 'axios';
+const oauth = require('axios-oauth-client');
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Ok';
+  }
+
+  getToken() {
+    const getClientCredentials = oauth.client(axios.create(), {
+      url: process.env.CHES_TOKEN_URL,
+      grant_type: 'client_credentials',
+      client_id: process.env.CHES_CLIENT_ID,
+      client_secret: process.env.CHES_CLIENT_SECRET,
+    });
+
+    return getClientCredentials()
+      .then((res) => {
+        if (res && res.access_token) return res.access_token;
+        else return null;
+      })
+      .catch((e) => {
+        throw new HttpException(
+          `Failed to get email auth token from API: ${e}`,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      });
   }
 }
